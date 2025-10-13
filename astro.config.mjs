5841ada714343a5ab2bd755e8fd2b4f8bbfeac50
nikos <nsdimitriadis@gmail.com>
2025-10-13 18:26:20 +0100

    loading fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,8 @@ import sitemap from '@astrojs/sitemap';
 const REPLIT_HOST = 'c611b145-39b5-4b6f-9fd3-5e1ef5ed92c1-00-e4c2l78mdzov.janeway.replit.dev';
 
 export default defineConfig({
-  site: 'https://nikos-dimitriadis.github.io',
-  // If deploying as a project page (repo: AstroPage), use base: '/AstroPage/'.
-  // If deploying as a user site (repo: nsdimitriadis.github.io), keep base: '/'.
-  base: process.env.GITHUB_REPOSITORY && !process.env.GITHUB_REPOSITORY.endsWith('.github.io')
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/',
+  site: 'https://nsdimitriadis.github.io',
+  base: '/AstroPage/',
   integrations: [
     tailwind(),
     react(),
